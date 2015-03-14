0e1b770a8995d72f9f16e1ea2d46e003c5f03e8b
Kevin Sullivan <admin@sideapps.com>
2015-03-14 12:43:17 -0400

    Project Commit
    Signed-off-by: Kevin Sullivan <admin@sideapps.com>

---
+++ BackItApp/BackItApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BackItApp
+//
+//  Created by Kevin Sullivan on 3/14/15.
+//  Copyright (c) 2015 SideApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
